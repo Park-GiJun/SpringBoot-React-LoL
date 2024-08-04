ad5024f74f5e735387f1ac09427e272493236f25
tpgj9 <tpgj98@naver.com>
2024-08-04 21:10:21 +0900

    채팅방 추가

--- src/main/java/com/example/springbootreactlol/controller/ChatController.java
+++ src/main/java/com/example/springbootreactlol/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.example.springbootreactlol.controller;
 
 import com.example.springbootreactlol.data.ChatMessage;
 import com.example.springbootreactlol.service.ChatService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
-
     private final ChatService chatService;
 
     public ChatController(ChatService chatService, SimpMessagingTemplate messagingTemplate) {
@@ -32,10 +30,17 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         chatService.addUser(chatMessage.getSender());
         sendConnectedUsers();
+
+        // 환영 메시지 추가
+        ChatMessage welcomeMessage = new ChatMessage();
+        welcomeMessage.setType(ChatMessage.MessageType.JOIN);
+        welcomeMessage.setSender("System");
+        welcomeMessage.setContent(chatMessage.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/topic/public", welcomeMessage);
+
         return chatMessage;
     }
 
@@ -44,11 +49,18 @@ public class ChatController {
     public ChatMessage removeUser(@Payload ChatMessage chatMessage) {
         chatService.removeUser(chatMessage.getSender());
         sendConnectedUsers();
+
+        // 퇴장 메시지 추가
+        ChatMessage leaveMessage = new ChatMessage();
+        leaveMessage.setType(ChatMessage.MessageType.LEAVE);
+        leaveMessage.setSender("System");
+        leaveMessage.setContent(chatMessage.getSender() + "님이 퇴장하셨습니다.");
+        messagingTemplate.convertAndSend("/topic/public", leaveMessage);
+
         return chatMessage;
     }
 
     private void sendConnectedUsers() {
-        messagingTemplate.convertAndSend("/topic/connectedUsers",
-                chatService.getConnectedUsers());
+        messagingTemplate.convertAndSend("/topic/connectedUsers", chatService.getConnectedUsers());
     }
 }
