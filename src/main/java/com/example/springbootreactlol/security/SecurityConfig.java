5c29b50a19b1382b4e792ae0f0bf6564ce512fed
tpgj9 <tpgj98@naver.com>
2024-08-10 13:30:49 +0900

    채팅방 추가

--- src/main/java/com/example/springbootreactlol/security/SecurityConfig.java
+++ src/main/java/com/example/springbootreactlol/security/SecurityConfig.java
@@ -96,7 +96,6 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://olm.life"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
